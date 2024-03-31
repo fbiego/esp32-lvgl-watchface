52f1aa2303d03b3035904fd919a7f694342fd745
Felix Biego <45796740+fbiego@users.noreply.github.com>
2024-03-31 17:52:44 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# ESP32 LVGL Watchfaces
+
+This project renders pre-built binary watchfaces on ESP32 using LVGL. A Kotlin script is used to transform the binary watchfaces into LVGL code, which is then compiled for the ESP32 display.
+
+## Watchfaces
+
+Watchfaces can be obtained from [watch-face-wearfit](https://github.com/fbiego/watch-face-wearfit) or [Chronos Dials](https://chronos.ke/dials). Since this project uses a 240x240 screen, watchfaces of the same resolution are recommended.
+
+#### Preview
+
+!["Analog"](src/faces/75_2_dial/watchface.png?raw=true "75_2_dial")
+!["Shadow"](src/faces/34_2_dial/watchface.png?raw=true "34_2_dial")
+!["Blue"](src/faces/79_2_dial/watchface.png?raw=true "79_2_dial")
+!["Radar"](src/faces/radar/watchface.png?raw=true "radar")
+!["Outline"](src/faces/116_2_dial/watchface.png?raw=true "116_2_dial")
+!["Red"](src/faces/756_2_dial/watchface.png?raw=true "756_2_dial")
+!["Tix"](src/faces/tix_resized/watchface.png?raw=true "tix_resized")
+!["Pixel"](src/faces/pixel_resized/watchface.png?raw=true "pixel_resized")
+!["Smart"](src/faces/smart_resized/watchface.png?raw=true "smart_resized")
+
+## Usage
+
+The Kotlin script [`bin2lvgl.kt`](src/faces/bin2lvgl.kt) has been compiled into `bin2lvgl.jar`.
+
+You can also recompile it with `kotlinc bin2lvgl.kt -include-runtime -d bin2lvgl.jar`.
+
+To convert the watchface, simply run `java -jar bin2lvgl.jar watchface.bin`, and LVGL code will be generated into a folder.
+
+In the `main.cpp`, include the `watchface.h` file.
+
+You will need to implement a callback for the watchface events:
+
+```
+void onFaceEvent(lv_event_t *e){
+
+}
+```
+After initializing LVGL, initialize the watchface:
+```
+init_face_watchface();
+lv_disp_load_scr(face_watchface); // load the watchface root object to display it
+```
+
+In the loop, use the functions to update the watchface:
+```
+update_time_watchface(sec, min, hr, md, am, dy, mt, yr, wk);
+update_weather_watchface(temp, ic);
+update_status_watchface(bat, con);
+update_activity_watchface(2735, 357, 345);
+update_health_watchface(76, 97);
+```
+
+This project implements using multiple watchfaces using a `face_selector` object. Note that the number of watchfaces you can compile is limited by the target ESP32 flash size.
+
+This project only demonstrates watchfaces that are functional to display time by leveraging the [`ChronosESP32 library`](https://github.com/fbiego/chronos-esp32).
+
+For other advanced features such as notifications and control, check out the [`esp32-c3-mini`](https://github.com/fbiego/esp32-c3-mini) project.
---
+++ src/main.cpp
@@ -0,0 +1,468 @@
+/*
+   MIT License
+
+  Copyright (c) 2024 Felix Biego
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+
+  ______________  _____
+  ___  __/___  /_ ___(_)_____ _______ _______
+  __  /_  __  __ \__  / _  _ \__  __ `/_  __ \
+  _  __/  _  /_/ /_  /  /  __/_  /_/ / / /_/ /
+  /_/     /_.___/ /_/   \___/ _\__, /  \____/
+                              /____/
+
+*/
+
+#define LGFX_USE_V1
+#include "Arduino.h"
+#include <lvgl.h>
+#include <LovyanGFX.hpp>
+#include "CST816D.h"
+#include <ChronosESP32.h>
+#include <Timber.h>
+
+#include "faces/34_2_dial/34_2_dial.h"
+#include "faces/75_2_dial/75_2_dial.h"
+#include "faces/79_2_dial/79_2_dial.h"
+#include "faces/radar/radar.h"
+#include "faces/116_2_dial/116_2_dial.h"
+#include "faces/756_2_dial/756_2_dial.h"
+
+#include "faces/tix_resized/tix_resized.h"
+#include "faces/pixel_resized/pixel_resized.h"
+#include "faces/smart_resized/smart_resized.h"
+
+#include "main.h"
+
+#define buf_size 10
+
+class LGFX : public lgfx::LGFX_Device
+{
+
+  lgfx::Panel_GC9A01 _panel_instance;
+  lgfx::Light_PWM _light_instance;
+  lgfx::Bus_SPI _bus_instance;
+
+public:
+  LGFX(void)
+  {
+    {
+      auto cfg = _bus_instance.config();
+
+      // SPIバスの設定
+      cfg.spi_host = SPI; // 使用するSPIを選択  ESP32-S2,C3 : SPI2_HOST or SPI3_HOST / ESP32 : VSPI_HOST or HSPI_HOST
+      // ※ ESP-IDFバージョンアップに伴い、VSPI_HOST , HSPI_HOSTの記述は非推奨になるため、エラーが出る場合は代わりにSPI2_HOST , SPI3_HOSTを使用してください。
+      cfg.spi_mode = 0;                  // SPI通信モードを設定 (0 ~ 3)
+      cfg.freq_write = 80000000;         // 传输时的SPI时钟（最高80MHz，四舍五入为80MHz除以整数得到的值）
+      cfg.freq_read = 20000000;          // 接收时的SPI时钟
+      cfg.spi_3wire = true;              // 受信をMOSIピンで行う場合はtrueを設定
+      cfg.use_lock = true;               // 使用事务锁时设置为 true
+      cfg.dma_channel = SPI_DMA_CH_AUTO; // 使用するDMAチャンネルを設定 (0=DMA不使用 / 1=1ch / 2=ch / SPI_DMA_CH_AUTO=自動設定)
+      // ※ ESP-IDFバージョンアップに伴い、DMAチャンネルはSPI_DMA_CH_AUTO(自動設定)が推奨になりました。1ch,2chの指定は非推奨になります。
+      cfg.pin_sclk = SCLK; // SPIのSCLKピン番号を設定
+      cfg.pin_mosi = MOSI; // SPIのCLKピン番号を設定
+      cfg.pin_miso = MISO; // SPIのMISOピン番号を設定 (-1 = disable)
+      cfg.pin_dc = DC;     // SPIのD/Cピン番号を設定  (-1 = disable)
+
+      _bus_instance.config(cfg);              // 設定値をバスに反映します。
+      _panel_instance.setBus(&_bus_instance); // バスをパネルにセットします。
+    }
+
+    {                                      // 表示パネル制御の設定を行います。
+      auto cfg = _panel_instance.config(); // 表示パネル設定用の構造体を取得します。
+
+      cfg.pin_cs = CS;   // CSが接続されているピン番号   (-1 = disable)
+      cfg.pin_rst = RST; // RSTが接続されているピン番号  (-1 = disable)
+      cfg.pin_busy = -1; // BUSYが接続されているピン番号 (-1 = disable)
+
+      // ※ 以下の設定値はパネル毎に一般的な初期値が設定さ BUSYが接続されているピン番号 (-1 = disable)れていますので、不明な項目はコメントアウトして試してみてください。
+
+      cfg.memory_width = 240;   // ドライバICがサポートしている最大の幅
+      cfg.memory_height = 240;  // ドライバICがサポートしている最大の高さ
+      cfg.panel_width = 240;    // 実際に表示可能な幅
+      cfg.panel_height = 240;   // 実際に表示可能な高さ
+      cfg.offset_x = 0;         // パネルのX方向オフセット量
+      cfg.offset_y = 0;         // パネルのY方向オフセット量
+      cfg.offset_rotation = 0;  // 值在旋转方向的偏移0~7（4~7是倒置的）
+      cfg.dummy_read_pixel = 8; // 在读取像素之前读取的虚拟位数
+      cfg.dummy_read_bits = 1;  // 读取像素以外的数据之前的虚拟读取位数
+      cfg.readable = false;     // 如果可以读取数据，则设置为 true
+      cfg.invert = true;        // 如果面板的明暗反转，则设置为 true
+      cfg.rgb_order = false;    // 如果面板的红色和蓝色被交换，则设置为 true
+      cfg.dlen_16bit = false;   // 对于以 16 位单位发送数据长度的面板，设置为 true
+      cfg.bus_shared = false;   // 如果总线与 SD 卡共享，则设置为 true（使用 drawJpgFile 等执行总线控制）
+
+      _panel_instance.config(cfg);
+    }
+
+    {                                      // Set backlight control. (delete if not necessary)
+      auto cfg = _light_instance.config(); // Get the structure for backlight configuration.
+
+      cfg.pin_bl = BL;     // pin number to which the backlight is connected
+      cfg.invert = false;  // true to invert backlight brightness
+      cfg.freq = 44100;    // backlight PWM frequency
+      cfg.pwm_channel = 1; // PWM channel number to use
+
+      _light_instance.config(cfg);
+      _panel_instance.setLight(&_light_instance); // Sets the backlight to the panel.
+    }
+
+    setPanel(&_panel_instance); // 使用するパネルをセットします。
+                                //    { // バックライト制御の設定を行います。(必要なければ削除）
+                                //    auto cfg = _light_instance.config();// バックライト設定用の構造体を取得します。
+                                //    cfg.pin_bl = 8;             // バックライトが接続されているピン番号 BL
+                                //    cfg.invert = false;          // バックライトの輝度を反転させる場合 true
+                                //    cfg.freq   = 44100;          // バックライトのPWM周波数
+                                //    cfg.pwm_channel = 7;         // 使用するPWMのチャンネル番号
+                                //    _light_instance.config(cfg);
+                                //    _panel_instance.setLight(&_light_instance);//バックライトをパネルにセットします。
+                                //    }
+  }
+};
+
+LGFX tft;
+CST816D touch(I2C_SDA, I2C_SCL, TP_RST, TP_INT);
+ChronosESP32 watch("Chronos Watchface");
+
+static const uint32_t screenWidth = 240;
+static const uint32_t screenHeight = 240;
+
+static lv_disp_draw_buf_t draw_buf;
+static lv_color_t buf[2][screenWidth * buf_size];
+
+lv_obj_t *ui_faceSelect;
+
+
+struct Face{
+  const char *name; // watchface name
+  const lv_img_dsc_t *preview; // watchface preview image
+  lv_obj_t **watchface; // watchface root object pointer
+};
+
+Face face[] = {
+  {.name = "Analog", .preview = &face_75_2_dial_dial_img_preview_0, .watchface = &face_75_2_dial},
+  {.name = "Shadow", .preview = &face_34_2_dial_dial_img_preview_0, .watchface = &face_34_2_dial},
+  {.name = "Blue", .preview = &face_79_2_dial_dial_img_preview_0, .watchface = &face_79_2_dial},
+  {.name = "Radar", .preview = &face_radar_dial_img_preview_0, .watchface = &face_radar},
+  {.name = "Outline", .preview = &face_116_2_dial_dial_img_preview_0, .watchface = &face_116_2_dial},
+  {.name = "Red", .preview = &face_756_2_dial_dial_img_preview_0, .watchface = &face_756_2_dial},
+  {.name = "Tix", .preview = &face_tix_resized_dial_img_preview_0, .watchface = &face_tix_resized},
+  {.name = "Pixel", .preview = &face_pixel_resized_dial_img_preview_0, .watchface = &face_pixel_resized},
+  {.name = "Smart", .preview = &face_smart_resized_dial_img_preview_0, .watchface = &face_smart_resized},
+};
+
+size_t faceSize = sizeof(face) / sizeof(face[0]);
+
+void update_faces();
+
+/* Display flushing */
+void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
+{
+  if (tft.getStartCount() == 0)
+  {
+    tft.endWrite();
+  }
+
+  tft.pushImageDMA(area->x1, area->y1, area->x2 - area->x1 + 1, area->y2 - area->y1 + 1, (lgfx::swap565_t *)&color_p->full);
+  lv_disp_flush_ready(disp); /* tell lvgl that flushing is done */
+}
+
+/*Read the touchpad*/
+void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
+{
+
+  bool touched;
+  uint8_t gesture;
+  uint16_t touchX, touchY;
+
+  touched = touch.getTouch(&touchX, &touchY, &gesture);
+
+  if (!touched)
+  {
+    data->state = LV_INDEV_STATE_REL;
+  }
+  else
+  {
+    data->state = LV_INDEV_STATE_PR;
+
+    /*Set the coordinates*/
+    data->point.x = touchX;
+    data->point.y = touchY;
+  }
+}
+
+void onFaceSelected(lv_event_t *e){
+  lv_event_code_t event_code = lv_event_get_code(e);
+  lv_obj_t *target = lv_event_get_target(e);
+  int index = (int)lv_event_get_user_data(e);
+
+  if (event_code == LV_EVENT_CLICKED)
+  {
+    
+    lv_scr_load_anim(*face[index].watchface, LV_SCR_LOAD_ANIM_FADE_ON, 500, 0, false);
+  }
+}
+
+void onFaceEvent(lv_event_t *e){
+  lv_event_code_t event_code = lv_event_get_code(e);
+  lv_obj_t *target = lv_event_get_target(e);
+
+  if (event_code == LV_EVENT_LONG_PRESSED)
+  {
+    lv_scr_load_anim(ui_faceSelect, LV_SCR_LOAD_ANIM_FADE_ON, 500, 0, false);
+  }
+  
+}
+
+void addWatchface(const char* name, const lv_img_dsc_t *src, int index){
+
+  lv_obj_t *ui_faceItem = lv_obj_create(ui_faceSelect);
+  lv_obj_set_width( ui_faceItem, 160);
+  lv_obj_set_height( ui_faceItem, 180);
+  lv_obj_set_align( ui_faceItem, LV_ALIGN_CENTER );
+  lv_obj_clear_flag( ui_faceItem, LV_OBJ_FLAG_SCROLLABLE );    /// Flags
+  lv_obj_set_style_radius(ui_faceItem, 0, LV_PART_MAIN| LV_STATE_DEFAULT);
+  lv_obj_set_style_bg_color(ui_faceItem, lv_color_hex(0x000000), LV_PART_MAIN | LV_STATE_DEFAULT );
+  lv_obj_set_style_bg_opa(ui_faceItem, 255, LV_PART_MAIN| LV_STATE_DEFAULT);
+  lv_obj_set_style_border_width(ui_faceItem, 0, LV_PART_MAIN| LV_STATE_DEFAULT);
+  lv_obj_set_style_outline_color(ui_faceItem, lv_color_hex(0x142ABC), LV_PART_MAIN | LV_STATE_DEFAULT );
+  lv_obj_set_style_outline_opa(ui_faceItem, 255, LV_PART_MAIN| LV_STATE_DEFAULT);
+  lv_obj_set_style_outline_width(ui_faceItem, 2, LV_PART_MAIN| LV_STATE_DEFAULT);
+  lv_obj_set_style_outline_pad(ui_faceItem, 1, LV_PART_MAIN| LV_STATE_DEFAULT);
+  lv_obj_set_style_pad_left(ui_faceItem, 0, LV_PART_MAIN| LV_STATE_DEFAULT);
+  lv_obj_set_style_pad_right(ui_faceItem, 0, LV_PART_MAIN| LV_STATE_DEFAULT);
+  lv_obj_set_style_pad_top(ui_faceItem, 0, LV_PART_MAIN| LV_STATE_DEFAULT);
+  lv_obj_set_style_pad_bottom(ui_faceItem, 0, LV_PART_MAIN| LV_STATE_DEFAULT);
+
+  lv_obj_t *ui_facePreview = lv_img_create(ui_faceItem);
+  lv_img_set_src(ui_facePreview, src);
+  lv_obj_set_width( ui_facePreview, LV_SIZE_CONTENT);  /// 1
+  lv_obj_set_height( ui_facePreview, LV_SIZE_CONTENT);   /// 1
+  lv_obj_set_align( ui_facePreview, LV_ALIGN_TOP_MID );
+  lv_obj_add_flag( ui_facePreview, LV_OBJ_FLAG_ADV_HITTEST );   /// Flags
+  lv_obj_clear_flag( ui_facePreview, LV_OBJ_FLAG_SCROLLABLE );    /// Flags
+
+  lv_obj_t *ui_faceLabel = lv_label_create(ui_faceItem);
+  lv_obj_set_width( ui_faceLabel, 160);
+  lv_obj_set_height( ui_faceLabel, LV_SIZE_CONTENT);   /// 1
+  lv_obj_set_align( ui_faceLabel, LV_ALIGN_BOTTOM_MID );
+  lv_label_set_long_mode(ui_faceLabel,LV_LABEL_LONG_DOT);
+  lv_label_set_text(ui_faceLabel, name);
+  lv_obj_set_style_text_align(ui_faceLabel, LV_TEXT_ALIGN_CENTER, LV_PART_MAIN| LV_STATE_DEFAULT);
+  lv_obj_set_style_text_font(ui_faceLabel, &lv_font_montserrat_16, LV_PART_MAIN| LV_STATE_DEFAULT);
+
+  lv_obj_add_event_cb(ui_faceItem, onFaceSelected, LV_EVENT_ALL, (void *)index);
+
+}
+
+void init_face_select(){
+  ui_faceSelect = lv_obj_create(NULL);
+  lv_obj_set_width( ui_faceSelect, 240);
+  lv_obj_set_height( ui_faceSelect, 240);
+  lv_obj_set_align( ui_faceSelect, LV_ALIGN_CENTER );
+  lv_obj_set_flex_flow(ui_faceSelect,LV_FLEX_FLOW_ROW);
+  lv_obj_set_flex_align(ui_faceSelect, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
+  lv_obj_clear_flag( ui_faceSelect, LV_OBJ_FLAG_SNAPPABLE );    /// Flags
+  lv_obj_set_scrollbar_mode(ui_faceSelect, LV_SCROLLBAR_MODE_OFF);
+  lv_obj_set_style_radius(ui_faceSelect, 0, LV_PART_MAIN| LV_STATE_DEFAULT);
+  lv_obj_set_style_bg_color(ui_faceSelect, lv_color_hex(0x000000), LV_PART_MAIN | LV_STATE_DEFAULT );
+  lv_obj_set_style_bg_opa(ui_faceSelect, 255, LV_PART_MAIN| LV_STATE_DEFAULT);
+  lv_obj_set_style_border_width(ui_faceSelect, 0, LV_PART_MAIN| LV_STATE_DEFAULT);
+  lv_obj_set_style_pad_left(ui_faceSelect, 30, LV_PART_MAIN| LV_STATE_DEFAULT);
+  lv_obj_set_style_pad_right(ui_faceSelect, 30, LV_PART_MAIN| LV_STATE_DEFAULT);
+  lv_obj_set_style_pad_top(ui_faceSelect, 0, LV_PART_MAIN| LV_STATE_DEFAULT);
+  lv_obj_set_style_pad_bottom(ui_faceSelect, 0, LV_PART_MAIN| LV_STATE_DEFAULT);
+  lv_obj_set_style_pad_row(ui_faceSelect, 10, LV_PART_MAIN| LV_STATE_DEFAULT);
+  lv_obj_set_style_pad_column(ui_faceSelect, 15, LV_PART_MAIN| LV_STATE_DEFAULT);
+
+  for (int x = 0; x < faceSize; x++){
+    addWatchface(face[x].name, face[x].preview, x);
+  }
+}
+
+void logCallback(Level level, unsigned long time, String message)
+{
+  Serial.print(message);
+}
+
+
+void setup() {
+  Serial.begin(115200); /* prepare for possible serial debug */
+
+  Timber.setLogCallback(logCallback);
+
+  Timber.i("Starting up device");
+
+
+  tft.init();
+  tft.initDMA();
+  tft.startWrite();
+
+  touch.begin();
+  lv_init();
+
+  lv_disp_draw_buf_init(&draw_buf, buf[0], buf[1], screenWidth * buf_size);
+
+  /*Initialize the display*/
+  static lv_disp_drv_t disp_drv;
+  lv_disp_drv_init(&disp_drv);
+  /*Change the following line to your display resolution*/
+  disp_drv.hor_res = screenWidth;
+  disp_drv.ver_res = screenHeight;
+  disp_drv.flush_cb = my_disp_flush;
+  disp_drv.draw_buf = &draw_buf;
+  lv_disp_drv_register(&disp_drv);
+
+  /*Initialize the (dummy) input device driver*/
+  static lv_indev_drv_t indev_drv;
+  lv_indev_drv_init(&indev_drv);
+  indev_drv.type = LV_INDEV_TYPE_POINTER;
+  indev_drv.read_cb = my_touchpad_read;
+  lv_indev_drv_register(&indev_drv);
+
+  lv_disp_t *dispp = lv_disp_get_default();
+  lv_theme_t *theme = lv_theme_default_init(dispp, lv_palette_main(LV_PALETTE_BLUE), lv_palette_main(LV_PALETTE_RED), true, LV_FONT_DEFAULT);
+  lv_disp_set_theme(dispp, theme);
+
+
+  init_face_34_2_dial();
+  init_face_75_2_dial();
+  init_face_79_2_dial();
+  init_face_radar();
+  init_face_116_2_dial();
+  init_face_756_2_dial();
+
+  init_face_smart_resized();
+  init_face_tix_resized();
+  init_face_pixel_resized();
+
+  init_face_select();
+
+  lv_disp_load_scr(face_75_2_dial);
+
+  
+  // lv_obj_t *preview = lv_img_create(lv_scr_act());
+  // lv_img_set_src(preview, &face_79_2_dial_dial_img_preview_0);
+  // lv_obj_set_width(preview, LV_SIZE_CONTENT);
+  // lv_obj_set_height(preview, LV_SIZE_CONTENT);
+  // lv_obj_set_x(preview, 40);
+  // lv_obj_set_y(preview, 40);
+  // lv_obj_add_flag(preview, LV_OBJ_FLAG_ADV_HITTEST );
+  // lv_obj_clear_flag(preview, LV_OBJ_FLAG_SCROLLABLE );
+
+  // watch.setConnectionCallback(connectionCallback);
+  // watch.setNotificationCallback(notificationCallback);
+  // watch.setConfigurationCallback(configCallback);
+  watch.begin();
+  watch.set24Hour(true);
+  watch.setBattery(70);
+
+  tft.setBrightness(200);
+
+}
+
+void loop() {
+  
+  lv_timer_handler(); /* let the GUI do its work */
+  delay(5);
+
+  watch.loop();
+
+  update_faces();
+
+  
+
+  
+
+}
+
+void update_faces(){
+  int sec = watch.getSecond();
+  int min = watch.getMinute();
+  int hr = watch.getHourC();
+  bool md =  watch.is24Hour();
+  bool am = watch.getHour(true) < 12;
+  int dy = watch.getDay();
+  int mt = watch.getMonth() + 1;
+  int yr = watch.getYear();
+  int wk = watch.getDayofWeek();
+
+  int temp = watch.getWeatherAt(0).temp;
+  int ic =  watch.getWeatherAt(0).icon;
+
+  int bat = watch.getPhoneBattery();
+  int con =  watch.isConnected();
+
+  update_time_34_2_dial(sec, min, hr, md, am, dy, mt, yr, wk);
+  update_weather_34_2_dial(temp, ic);
+  update_status_34_2_dial(bat, con);
+  update_activity_34_2_dial(2735, 357, 345);
+  update_health_34_2_dial(76, 97);
+
+
+  update_time_75_2_dial(sec, min, hr, md, am, dy, mt, yr, wk);
+  update_weather_75_2_dial(temp, ic);
+  update_status_75_2_dial(bat, con);
+  update_activity_75_2_dial(2735, 357, 345);
+  update_health_75_2_dial(76, 97);
+
+  update_time_79_2_dial(sec, min, hr, md, am, dy, mt, yr, wk);
+  update_weather_79_2_dial(temp, ic);
+  update_status_79_2_dial(bat, con);
+  update_activity_79_2_dial(2735, 357, 345);
+  update_health_79_2_dial(76, 97);
+
+  update_time_radar(sec, min, hr, md, am, dy, mt, yr, wk);
+  update_weather_radar(temp, ic);
+  update_status_radar(bat, con);
+  update_activity_radar(2735, 357, 345);
+  update_health_radar(76, 97);
+
+  update_time_116_2_dial(sec, min, hr, md, am, dy, mt, yr, wk);
+  update_weather_116_2_dial(temp, ic);
+  update_status_116_2_dial(bat, con);
+  update_activity_116_2_dial(2735, 357, 345);
+  update_health_116_2_dial(76, 97);
+
+  update_time_756_2_dial(sec, min, hr, md, am, dy, mt, yr, wk);
+  update_weather_756_2_dial(temp, ic);
+  update_status_756_2_dial(bat, con);
+  update_activity_756_2_dial(2735, 357, 345);
+  update_health_756_2_dial(76, 97);
+
+  update_time_tix_resized(sec, min, hr, md, am, dy, mt, yr, wk);
+  update_weather_tix_resized(temp, ic);
+  update_status_tix_resized(bat, con);
+  update_activity_tix_resized(2735, 357, 345);
+  update_health_tix_resized(76, 97);
+
+  update_time_pixel_resized(sec, min, hr, md, am, dy, mt, yr, wk);
+  update_weather_pixel_resized(temp, ic);
+  update_status_pixel_resized(bat, con);
+  update_activity_pixel_resized(2735, 357, 345);
+  update_health_pixel_resized(76, 97);
+
+  update_time_smart_resized(sec, min, hr, md, am, dy, mt, yr, wk);
+  update_weather_smart_resized(temp, ic);
+  update_status_smart_resized(bat, con);
+  update_activity_smart_resized(2735, 357, 345);
+  update_health_smart_resized(76, 97);
+
+}
