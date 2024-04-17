96db2d2fb460442de75d97c9d49b4d6b6f604491
Daniel Kampert <DanielKampert@Kampis-Elektroecke.de>
2024-04-17 17:31:31 +0200

    Update license

--- bin2lvgl.kt
+++ bin2lvgl.kt
@@ -2,6 +2,7 @@
    MIT License
 
   Copyright (c) 2024 Felix Biego
+  Modified (c) 2024 Daniel Kampert
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
