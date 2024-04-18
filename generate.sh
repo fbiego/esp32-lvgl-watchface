d4ca49b4fe9e4b7b990abf418f99c8acd758fcb7
Daniel Kampert <DanielKampert@Kampis-Elektroecke.de>
2024-04-18 09:45:17 +0200

    Move downloaded files into a download directory, add support for separate asset directory

--- generate.sh
+++ generate.sh
@@ -12,6 +12,23 @@ ZSWATCH_ROOT=/home/daniel/ZSWatch
 FILENAME=${URL##*/}
 FILENAME_WITHOUT_ENDING=${FILENAME%%.*}
 
-echo "Download watchface ${FILENAME}"
-wget $1
+if test -f "download/${FILENAME}"; then
+    echo "Watchface ${FILENAME} exists. Use file from download directory"
+    mv download/${FILENAME} ${FILENAME} 
+else
+    echo "Download watchface ${FILENAME}"
+    wget ${1}
+fi
+
+mkdir -p ${ZSWATCH_ROOT}/app/src/images/assets_watchfaces
+
+# Generate the code
 kotlinc bin2lvgl.kt -include-runtime -d bin2lvgl.jar && java -jar bin2lvgl.jar ${FILENAME} && mkdir -p ${ZSWATCH_ROOT}/app/src/ui/watchfaces/${FILENAME_WITHOUT_ENDING} && cp ${FILENAME_WITHOUT_ENDING}/* ${ZSWATCH_ROOT}/app/src/ui/watchfaces/${FILENAME_WITHOUT_ENDING}/
+mv ${FILENAME} download/${FILENAME}
+
+# Copy all assets into the ZSWatch project
+cd assets_watchfaces
+mv */* .
+rm -r ${FILENAME_WITHOUT_ENDING}
+cd ..
+mv assets_watchfaces/* ${ZSWATCH_ROOT}/app/src/images/assets_watchfaces
